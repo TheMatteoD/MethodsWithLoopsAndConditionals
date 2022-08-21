9a9d07f5ea56504b86266a49209f55d5299d56ae
Matteo DeSimone <thematteod@gmail.com>
2022-08-20 20:22:32 -0400

    Cleaned up

--- IterationStatementsProject/Program.cs
+++ IterationStatementsProject/Program.cs
@@ -5,6 +5,7 @@ namespace IterationStatementsProject
     internal class Program
     {
         public static void Negative1000ToPositive1000(){
+            Console.WriteLine("First I will count from -1000 to 1000:");
             int number = -1000;
             do
             {
@@ -14,6 +15,7 @@ namespace IterationStatementsProject
         }
         public static void CountBy3To999()
         {
+            Console.WriteLine("Next I will count from 3 to 999 counting by 3s:");
             int number = 0;
             do
             {
@@ -21,49 +23,91 @@ namespace IterationStatementsProject
                 Console.WriteLine(number);
             }while(number < 999);
         }
-        public static bool CheckForSameNumber(int a, int b)
+        public static void CheckForSameNumber()
         {
-            if (a == b)
-                return true;
+            Console.WriteLine("Now you can enter two numbers and I will check if you entered the same number twice");
+            Console.WriteLine("Enter your first number now:");
+            int a;
+            var response = int.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Enter your second number now:");
+            int b;
+            var response2 = int.TryParse(Console.ReadLine(), out b);
+            if (response == true && response2 == true)
+                if (a == b)
+                    Console.WriteLine("They were the same number!");
+                else
+                    Console.WriteLine("They were different numbers!");
             else
-                return false;
+                Console.WriteLine("You did not input valid numbers!");
         }
-        public static void EvenOrOdd(int a)
+        public static void EvenOrOdd()
         {
-            if (a % 2 == 0)
-                Console.WriteLine("It's Even!");
+            Console.WriteLine("Input a number and I can tell you if it's even or odd:");
+            int evenOddInput;
+            var response = int.TryParse(Console.ReadLine(), out evenOddInput);
+            if (response)
+                if (evenOddInput % 2 == 0)
+                    Console.WriteLine("It's Even!");
+                else
+                    Console.WriteLine("It's Odd!");
             else
-                Console.WriteLine("It's Odd!");
+                Console.WriteLine("You did not enter a valid number.");
         }
-        public static void PositiveOrNegative(int a)
+        public static void PositiveOrNegative()
         {
-            if (a > 0)
-                Console.WriteLine("It is positive!");
-            else if (a < 0)
-                Console.WriteLine("It is negative!");
+            Console.WriteLine("Input a number and I can tell you if it's positive or negative:");
+            int positiveNegativeInput;
+            var response = int.TryParse(Console.ReadLine(), out positiveNegativeInput);
+            if (response)
+                if (positiveNegativeInput > 0)
+                    Console.WriteLine("It is positive!");
+                else if (positiveNegativeInput < 0)
+                    Console.WriteLine("It is negative!");
+                else
+                    Console.WriteLine("You entered zero.");
             else
-                Console.WriteLine("You entered zero.");
+                Console.WriteLine("You did not enter a valid number.");
         }
-        public static void CheckVotingAge(int age)
+        public static void CheckVotingAge()
         {
-            if (age >= 18)
-                Console.WriteLine("You are old enough to vote.");
+            Console.WriteLine("If you tell me your age; I can tell you if you are old enough to vote.");
+            Console.WriteLine("Enter your age:");
+            int age;
+            var response = int.TryParse(Console.ReadLine(), out age);
+            if (response)
+                if (age >= 18)
+                    Console.WriteLine("You are old enough to vote.");
+                else
+                    Console.WriteLine("You are not old enough to vote.");
             else
-                Console.WriteLine("You are not old enough to vote.");
+                Console.WriteLine("You did not enter a valid number.");
         }
-        public static void IsAbsoluteValueMoreThan10(int a)
+        public static void IsAbsoluteValueMoreThan10()
         {
-            if (a >= -10 && a <= 10)
-                Console.WriteLine("Your number is within -10 and 10.");
-            else if (a < -10)
-                Console.WriteLine("Your number is less than -10");
+            Console.WriteLine("Enter a number and I will tell you if its between -10 and 10.");
+            Console.WriteLine("Number:");
+            int number;
+            var response = int.TryParse(Console.ReadLine(), out number);
+            if (response)
+                if (number >= -10 && number <= 10)
+                   Console.WriteLine("Your number is within -10 and 10.");
+                else if (number < -10)
+                    Console.WriteLine("Your number is less than -10");
+                else
+                    Console.WriteLine("Your number is greater than 10");
             else
-                Console.WriteLine("Your number is greater than 10");
+                Console.WriteLine("You did not enter a valid number.");
         }
-        public static void MultiplicationTable(int a)
-        {
-            for (int i = 1; i < 13; i++)
-                Console.WriteLine($"{i} x {a} = {i * a}");
+        public static void MultiplicationTable()
+        { 
+            Console.WriteLine("Enter a number to see its multiplication table:");
+            int multiplier;
+            var response = int.TryParse(Console.ReadLine(), out multiplier);
+            if (response)
+                for (int i = 1; i < 13; i++)
+                    Console.WriteLine($"{i} x {multiplier} = {i * multiplier}");
+            else
+                Console.WriteLine("You did not enter a valid number."); 
         }
         static void Main(string[] args)
         {
@@ -76,77 +120,22 @@ namespace IterationStatementsProject
             //Exercise 2 was a forked repo
 
             //Exercise 3
-            Console.WriteLine("First I will count from -1000 to 1000:");
             Negative1000ToPositive1000();
             
-
-            Console.WriteLine("Next I will count from 3 to 999 counting by 3s:");
             CountBy3To999();
 
+            CheckForSameNumber();
 
-            Console.WriteLine("Now you can enter two numbers and I will check if you entered the same number twice");
-            Console.WriteLine("Enter your first number now:");
-            int a;
-            var response = int.TryParse(Console.ReadLine(), out a);
-            Console.WriteLine("Enter your second number now:");
-            int b;
-            var response2 = int.TryParse(Console.ReadLine(), out b);
-            if (response == true && response2 == true)
-                if (CheckForSameNumber(a, b))
-                    Console.WriteLine("They were the same number!");
-                else
-                    Console.WriteLine("They were different numbers!");
-            else
-                Console.WriteLine("You did not input valid numbers!");
+            EvenOrOdd();
 
+            PositiveOrNegative();
 
-            Console.WriteLine("Input a number and I can tell you if it's even or odd:");
-            int evenOddInput;
-            var response3 = int.TryParse(Console.ReadLine(), out evenOddInput);
-            if (response3)
-                EvenOrOdd(evenOddInput);
-            else
-                Console.WriteLine("You did not enter a valid number.");
+            CheckVotingAge();
 
+            IsAbsoluteValueMoreThan10();
 
-            Console.WriteLine("Input a number and I can tell you if it's positive or negative:");
-            int positiveNegativeInput;
-            var response4 = int.TryParse(Console.ReadLine(), out positiveNegativeInput);
-            if (response4)
-                PositiveOrNegative(positiveNegativeInput);
-            else
-                Console.WriteLine("You did not enter a valid number.");
+            MultiplicationTable();
 
-
-            Console.WriteLine("If you tell me your age; I can tell you if you are old enough to vote.");
-            Console.WriteLine("Enter your age:");
-            int age;
-            var response5 = int.TryParse(Console.ReadLine(), out age);
-            if (response5)
-                CheckVotingAge(age);
-            else
-                Console.WriteLine("You did not enter a valid number.");
-
-
-            Console.WriteLine("Enter a number and I will tell you if its between -10 and 10.");
-            Console.WriteLine("Number:");
-            int number;
-            var response6 = int.TryParse(Console.ReadLine(), out number);
-            if (response6)
-                IsAbsoluteValueMoreThan10(number);
-            else
-                Console.WriteLine("You did not enter a valid number.");
-
-
-
-            Console.WriteLine("Enter a number to see its multiplication table:");
-            int multiplier;
-            var response7 = int.TryParse(Console.ReadLine(), out multiplier);
-            if (response7)
-                MultiplicationTable(multiplier);
-            else
-                Console.WriteLine("You did not enter a valid number.");
-            
         }
     }
 }
